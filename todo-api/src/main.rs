50982273e3e4046adc3158ce1a52ec491bde9d79
佐藤昭文 <akfm.sato@gmail.com>
2022-08-09 15:08:12 +0900

    chapter 3.2: loggingの追加

--- todo-api/src/main.rs
+++ todo-api/src/main.rs
@@ -1,10 +1,16 @@
 use axum::{routing::get, Router};
-use std::net::SocketAddr;
+use std::{net::SocketAddr, env};
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(root));
+    let log_level = env::var("RUST_LOG").unwrap_or("info".to_string());
+    env::set_var("RUST_LOG", log_level);
+    tracing_subscriber::fmt::init();
+
+    let app = Router::new()
+        .route("/", get(root));
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    tracing::debug!("listening on {}", addr);
 
     axum::Server::bind(&addr)
         .serve(app.into_make_service())
