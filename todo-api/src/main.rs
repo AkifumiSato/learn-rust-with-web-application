ae454bc4ed3b4b1b8e3cce99f404750a41b7628c
佐藤昭文 <akfm.sato@gmail.com>
2022-08-09 15:07:32 +0900

    chapter 3.2: hello, world

--- todo-api/src/main.rs
+++ todo-api/src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{routing::get, Router};
+use std::net::SocketAddr;
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(root));
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
+
+async fn root() -> &'static str {
+    "Hello, World!"
 }
