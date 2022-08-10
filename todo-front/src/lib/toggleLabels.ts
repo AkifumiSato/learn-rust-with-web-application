e2dbd1b3e007a75372cccfb80e863605566f5eae
佐藤昭文 <akfm.sato@gmail.com>
2022-08-10 17:36:15 +0900

    chapter 6.3: fix label選択のバグ

--- todo-front/src/lib/toggleLabels.ts
+++ todo-front/src/lib/toggleLabels.ts
@@ -2,5 +2,5 @@ import { Label } from '../types/todo'
 
 export const toggleLabels = (labels: Label[], target: Label) =>
   labels.find(({ id }) => id === target.id)
-    ? labels.filter(({ id }) => id === target.id)
+    ? labels.filter(({ id }) => id !== target.id)
     : [...labels, target]
